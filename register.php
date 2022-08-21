8cfc9d591585a3e239e658b3dac5acf614a6aeac
18301148Sipon <109475375+18301148Sipon@users.noreply.github.com>
2022-08-21 23:17:52 +0600

    Add files via upload

--- register.php
+++ register.php
@@ -1,37 +1,225 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-			<title> Signup Page </title>
-			<?php include '../View/UI.php';?>
-			
-		</head>
+<?php include_once 'helpers/helper.php'; ?>
 
-  <body>
-	
-	<section id = "section1">
-		<div class="col-md-10" style="text-align: center; font-size: 100px; color:#2d2244"> JebLu Airways </div>
-                <div class="col-md-10" style="text-align: center; font-size: 26px;"> SIGN UP </div>
-		
-		<form action='../Model/register_process.php' class="form_design" method="post" style="color:White">
-		<div class="col-md-10" style="text-align: center; font-size: 25px; color:#fbf7f7">
+<?php subview('header.php'); ?>
 
-		Name <input type="text" placeholder="Enter Name" name="fullname"> <br/>
-		Username <input type="text" placeholder="Enter Username" name="uname"> <br/>
-		Gender <input type="text" placeholder="Enter Gender" name="gen"> <br/>
-		Email <input type="text" placeholder="Enter Email ID" name="email"> <br/>
-		Credit Card No <input type="text" placeholder="Enter Credit Card No" name="card"> <br/>
-		Address <input type="text" placeholder="Enter Address" name="live"> <br/>
-		Date of Birth <input type="date" name="bd"> <br/> <br/>
-		Mobile Number <input type="number" placeholder="Enter Mobile Number" name="mob"> <br/>
-		Passport <input type="text" placeholder="Enter Passport" name="pport"> <br/> 
-		Password <input type="password" placeholder="Enter Password" name="pass"> <br/> 
+<style>
+
+body {
+    background: #bdc3c7;  
+background: -webkit-linear-gradient(to right, #2c3e50, #bdc3c7); 
+background: linear-gradient(to right, #2c3e50, #bdc3c7); 
 
 
-			<button type="submit" value="Add to Database">Sign Up</button> <br><br>
-        </div>               
-		</form>
-	</section>
-<?php include '../View/Footer.php';?>
-  </body> 
-</html>
+}
+@font-face {
+  font-family: 'product sans';
+  src: url('assets/css/Product Sans Bold.ttf');
+}
+h3 {
+    text-align: center;
+    /* font-family: 'Italianno', cursive; */
+    font-family: 'product sans', cursive;      
+    font-weight: normal;
+    font-size: 55px;
+    margin-top: 20px !important;   
+}
+
+input {
+    /* background-color: #F8F9FA !important; */
+    margin-bottom: 10px;
+    border :0px !important;
+    border-bottom: 2px solid #838383 !important;
+    color :#838383 !important;
+    border-radius: 0px !important;
+    font-weight: bold !important;
+  }
+  label {
+    color : #838383 !important;      
+    font-size: 19px;
+  }
+.register{
+    margin-top: 3%;
+    padding: 3%;
+}
+.register-left{
+    text-align: center;
+    color: #fff;
+    margin-top: 4%;
+}
+.register-left input{
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    width: 60%;
+    background: #f8f9fa;
+    font-weight: bold;
+    color: #383d41;
+    margin-top: 30%;
+    margin-bottom: 3%;
+    cursor: pointer;
+}
+.register-right{
+    background: #f8f9fa;
+    border-top-left-radius: 10% 50%;
+    border-bottom-left-radius: 10% 50%;
+}
+.register-left img{
+    margin-top: 15%;
+    margin-bottom: 5%;
+    width: 25%;
+    -webkit-animation: mover 2s infinite  alternate;
+    animation: mover 1s infinite  alternate;
+}
+@-webkit-keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+@keyframes mover {
+    0% { transform: translateY(0); }
+    100% { transform: translateY(-20px); }
+}
+.register-left p{
+    font-weight: lighter;
+    padding: 12%;
+    margin-top: -9%;
+}
+.register .register-form{
+    padding: 60px;
+    margin-top: 10%;
+}
+.btnRegister{
+    float: right;
+    margin-top: 10%;
+    border: none;
+    border-radius: 1.5rem;
+    padding: 2%;
+    background: #4e4e4e;
+    color: #fff;
+    font-weight: 600;
+    width: 50%;
+    cursor: pointer;
+}
+.register-heading{
+    text-align: center;
+    margin-top: 8%;
+    margin-bottom: -15%;
+    color: #495057;
+}
+</style>
+<?php
+if(isset($_GET['error'])) {
+    if($_GET['error'] === 'invalidemail') {
+        echo '<script>alert("Invalid email")</script>';
+    } else if($_GET['error'] === 'pwdnotmatch') {
+        echo '<script>alert("Passwords do not match")</script>';
+    } else if($_GET['error'] === 'sqlerror') {
+        echo"<script>alert('Database error')</script>";
+    } else if($_GET['error'] === 'usernameexists') {
+        echo"<script>alert('Username already exists')</script>";
+    } else if($_GET['error'] === 'emailexists') {
+        echo"<script>alert('Email already exists')</script>";
+    }
+}
+?>
+<link rel="stylesheet" href="assets/css/form.css">
+<main>
+<div class="container-fluid mt-0 register">
+<div class="row">
+    <!-- <div class="col-md-3 register-left">
+        
+        <h3>Welcome</h3>
+    </div> -->
+    <div class="col-md-1"></div>
+    <div class="col-md-10 register-right">
+        <div class="tab-content" id="myTabContent">
+            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                <h3 class="register-heading text-secondary">PASSENGER REGISTRATION</h3>
+                <div class="register-form">
+                <form method="POST" action="includes/register.inc.php">
+                    <div class="conrainer-fluid">
+                    <div class="row">
+                        <div class="col-1 p-0">
+                            <i class="fa fa-user text-secondary" 
+                                style="float: right;margin-top:35px;"></i>
+                        </div>
+                        <div class="col-md">
+                            <div class="input-group">
+                                <label for="username">Username</label>
+                                <input type="text" name="username" id="username" required />                                
+                            </div>  
+                        </div>
+                        <div class="col-1 p-0 mr-2">
+                            <i class="fa fa-envelope text-secondary" 
+                                style="float: right;margin-top:35px;"></i>
+                        </div>                    
+                        <div class="col-md">
+                            <div class="input-group">
+                                <label for="email_id">Email</label>
+                                <input type="text" name="email_id" id="email_id"
+                                        required>                                         
+                            </div>                                     
+                        </div>                        
+                    </div>
+                    <div class="form-row">
+                    <div class="col-1 p-0">
+                            <i class="fa fa-lock text-secondary" 
+                                style="float: right;margin-top:35px;"></i>
+                        </div>                        
+                        <div class="col-md">
+                            <div class="input-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password"            
+                                required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                title="Must contain at least one number and one uppercase and lowercase letter,
+                                and at least 8 or more characters" />                                        
+                            </div>                                     
+
+                        </div>
+                        <div class="col-1 p-0 mr-2">
+                            <i class="fa fa-lock text-secondary" 
+                                style="float: right;margin-top:35px;"></i>
+                        </div>                        
+                        <div class="col-md">
+                            <div class="input-group">
+                                <label for="password_repeat">Confirm password</label>
+                                <input type="password" name="password_repeat" 
+                                    id="password_repeat" required>
+                            </div>                                     
+                        </div>                                                                                                  
+                    </div>
+                    <div class="text-center">
+                        <button name="signup_submit" type="submit" 
+                            class="btn btn-info mt-5">
+                            <div style="font-size: 1.5rem;">
+                            <i class="fa fa-lg fa-arrow-right"></i> Complete  
+                            </div>
+                        </button>                            
+                    </div>
+                    </div>
+                </form>
+                </div>                        
+            </div>
+        </div>
+    </div> 
+    <div class="col-md-1"></div>          
+</div>
+</div>
+
+<?php subview('footer.php'); ?>
+<script>
+$(document).ready(function(){
+  $('.input-group input').focus(function(){
+    me = $(this) ;
+    $("label[for='"+me.attr('id')+"']").addClass("animate-label");
+  }) ;
+  $('.input-group input').blur(function(){
+    me = $(this) ;
+    if ( me.val() == ""){
+      $("label[for='"+me.attr('id')+"']").removeClass("animate-label");
+    }
+  }) ;
+
+});    
+</script>
+ 
+</main>
